15f148b8048e4ba27a291e0220125092b1c4aece
Will Maethner <32457351+wmaethner@users.noreply.github.com>
2021-01-08 18:28:33 -0600

    Add documentation to validation routine

--- validation/validation.go
+++ validation/validation.go
@@ -1,3 +1,7 @@
+// Currently the token array would continue to grow for as long as the server
+// was on. Purging them every once in a while would probably be needed or
+// a different method for validating the token.
+
 package validation
 
 import (
@@ -28,11 +32,13 @@ func ValidateLogin(hash string, token string) (bool, string) {
 	return true, ""
 }
 
+// validLoginCredentials confirms the login credentials are valid
 func validLoginCredentials(hash string) bool {
 	_, valid := credentialsAndLogins[hash]
 	return valid
 }
 
+// validToken confirms the token is the correct format
 func validToken(token string) bool {
 	// confirm token length
 	if len(token) != 4 {
@@ -48,6 +54,7 @@ func validToken(token string) bool {
 	return true
 }
 
+// tokenExists checks if the token was already used for the login hash
 func tokenExists(hash string, token string) bool {
 	tokens, valid := credentialsAndLogins[hash]
 
@@ -62,6 +69,7 @@ func tokenExists(hash string, token string) bool {
 	return false
 }
 
+// contains checks if the slice contains the given element
 func contains(tokenSlice []string, token string) bool {
 	for _, val := range tokenSlice {
 		if val == token {
